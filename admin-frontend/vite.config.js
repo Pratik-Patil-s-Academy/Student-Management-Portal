b0778f0eaba92a5b4655ddc3fdb09257ebcec5ae
Pratik Chavan <pratikpchavan75@gmail.com>
2026-02-22 02:15:27 +0530

    pwa

--- admin-frontend/vite.config.js
+++ admin-frontend/vite.config.js
@@ -3,7 +3,39 @@ import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
 
+import { VitePWA } from 'vite-plugin-pwa'
+
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['vite.svg', 'pwa-192x192.svg', 'pwa-512x512.svg'],
+      manifest: {
+        name: 'Student Management Portal Admin',
+        short_name: 'SMP Admin',
+        description: 'Admin Portal for Student Management System',
+        theme_color: '#ffffff',
+        background_color: '#ffffff',
+        display: 'standalone',
+        icons: [
+          {
+            src: 'pwa-192x192.svg',
+            sizes: '192x192',
+            type: 'image/svg+xml'
+          },
+          {
+            src: 'pwa-512x512.svg',
+            sizes: '512x512',
+            type: 'image/svg+xml'
+          }
+        ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,ico,png,svg}']
+      }
+    })
+  ],
 })
